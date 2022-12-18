1e786ba79cb22fd61688c2a9b38938cdc0351d8d
Olga Kosoplecheva <olgakosopleceva@gmail.ru>
2022-12-18 16:31:26 +0300

    HW_Task2.2 Done

--- Lesson2/HomeWork2/HW_Task2.2.py
+++ Lesson2/HomeWork2/HW_Task2.2.py
@@ -4,3 +4,24 @@
 #1 - 1 * 1, 2 - 1 * 2, 3 - 1 * 2 * 3, 4 - 1 * 2 * 3 * 4 и т.д.
 #- 4 -> [1, 2, 6, 24]
 #- 6 -> [1, 2, 6, 24, 120, 720]
+
+def CreatListBuMiltNum(l, num):
+    if num == 1:
+        l.pop(1)
+        return l
+    elif num == 2:
+        return l
+    else:
+        c = len(l)
+        while c < num:
+            l.append((c+1) * l[c-1])
+            c +=1
+        return l
+
+my_List = [1, 2]
+n = int(input('Введите натуральное число:'))
+if n >= 0:
+    res = CreatListBuMiltNum(my_List, n)
+    print(res)
+else:
+    print('Ведите число, больше 0')
