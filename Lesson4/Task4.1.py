ecc594d0f9e18720dd26d5110acd483b4f35ba89
Olga Kosoplecheva <olgakosopleceva@gmail.ru>
2022-12-28 20:55:11 +0300

    Task4.1 Done

--- Lesson4/Task4.1.py
+++ Lesson4/Task4.1.py
@@ -2,3 +2,18 @@
 # которая покажет наибольшее и наименьшее число.
 # в качестве разделител используйте пробел.
 
+def UserInput(start_str):
+    new_str = start_str.split()
+    lst = []
+    for i in range(len(new_str)):
+        new_str[i] = new_str[i].strip(",.?/;:!=+")
+        if new_str[i].replace('-', '', 1).isdigit():
+            lst.append(new_str[i])
+    return lst
+
+def UserOutPut(lst):
+    if lst:
+        return min(lst, key = int), max(lst, key = int)
+    return'Вы ввели некорректные данные'
+
+print(UserOutPut(UserInput(input(""))))
