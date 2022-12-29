b6f580219e57d77646be04c7a837bcc275ad4344
Olga Kosoplecheva <olgakosopleceva@gmail.ru>
2022-12-29 14:25:38 +0300

    Task4.2 Done1

--- Lesson4/Task4.2.py
+++ Lesson4/Task4.2.py
@@ -7,7 +7,7 @@ def Discrim(a, b, c):
     d = b**2-4*a*c
     with open ("File.txt", "a", encoding="utf-8") as my_File:
         my_File.write(f"{a}X2+{b}x+{c}=0\n")
-        if d > 0:
+        if d > 0 and a:
             x1 = round((-b + sqrt(d))/(2*a), 4)
             x2 = round((-b - sqrt(d))/(2*a), 4)
             my_File.write(f'Певый корень: {x1},\nВторой корень {x2}\n')
